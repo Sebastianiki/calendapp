d4deada40f3f2baf86da5c88fa1e8f98f600bc84
Sebastian <reyesebastian1709@gmail.com>
2022-03-15 20:42:17 -0300

    change express static change package json

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'))
+}
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
