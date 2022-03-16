35bc0162622b9fcf8b2adf57760e9821a8cfef18
Sebastian <reyesebastian1709@gmail.com>
2022-03-15 21:14:11 -0300

    refactor to use on heroku

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'))
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
 }
 
 app.get("/api", (req, res) => {
