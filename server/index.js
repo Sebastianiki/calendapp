9e4ba8ded9ddaaacfcac5348af448a77bb4758b2
Sebastian <reyesebastian1709@gmail.com>
2022-03-21 23:22:21 -0300

    fix navbar and index server to dont show error

--- server/index.js
+++ server/index.js
@@ -14,14 +14,15 @@ app.use(express.json({ extend: true }));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
 };
 
 app.use('/api/posts', postRoutes);
 app.use('/api/auth', authRoutes);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+
 
 const PORT = process.env.PORT || 5000;
 
