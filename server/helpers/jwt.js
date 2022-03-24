ddb78a4f7129e87c6b0e48859630c5f0d520bbb7
Sebastian <reyesebastian1709@gmail.com>
2022-03-24 17:57:49 -0300

    update jwt to 30d

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -3,8 +3,7 @@ const jwt = require('jsonwebtoken')
 exports.generateJWT = ( userId ) =>{
   return new Promise( (resolve, reject) => {
     const payload = { userId };
-    console.log(userId, process.env.SECRET_JWT_SEED);
-    jwt.sign( payload, process.env.SECRET_JWT_SEED, { expiresIn: '2h' } , (err, token) => {
+    jwt.sign( payload, process.env.SECRET_JWT_SEED, { expiresIn: '30d' } , (err, token) => {
       if(err){
         console.log(err);
         reject('No se pudo generar el token')
