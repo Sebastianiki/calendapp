b0ba131fb66b1ee82d93c1e43d1cc987f4bee548
Sebastian <reyesebastian1709@gmail.com>
2022-03-25 20:22:40 -0300

    delete log on server

--- server/models/index.js
+++ server/models/index.js
@@ -8,8 +8,6 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-console.log(process.env.DATABASE_URL);
-
 let sequelize;
 if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, config);
