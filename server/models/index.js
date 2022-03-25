48cc5ab9aa5f4b097b898e20ba4e048b2cc671c3
Sebastian <reyesebastian1709@gmail.com>
2022-03-25 17:12:03 -0300

    config db connection to use env var

--- server/models/index.js
+++ server/models/index.js
@@ -8,9 +8,11 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+console.log(process.env.DATABASE_URL);
+
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
