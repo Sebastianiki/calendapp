d95bf1ab2253915be052dd854707180019b9616d
Sebastian <reyesebastian1709@gmail.com>
2022-03-25 16:44:26 -0300

    update build on production

--- server/index.js
+++ server/index.js
@@ -14,9 +14,6 @@ app.use(express.json({ extend: true }));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, '../client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
 };
 
 app.use('/api/auth', authRoutes);
