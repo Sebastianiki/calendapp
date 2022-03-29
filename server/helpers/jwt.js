c9296c370c2d7e054aa75948bd829fce93956678
Sebastian <reyesebastian1709@gmail.com>
2022-03-29 14:53:23 -0300

    change jwt to 48hr

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 exports.generateJWT = ( userId ) =>{
   return new Promise( (resolve, reject) => {
     const payload = { userId };
-    jwt.sign( payload, process.env.SECRET_JWT_SEED, { expiresIn: '30d' } , (err, token) => {
+    jwt.sign( payload, process.env.SECRET_JWT_SEED, { expiresIn: '48h' } , (err, token) => {
       if(err){
         console.log(err);
         reject('No se pudo generar el token')
